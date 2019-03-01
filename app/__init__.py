7c1955df0ae19240985a90e6adad5557ff70fd8e
Ben <benmaciorowski@yahoo.com>
2019-03-01 16:34:53 -0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,5 @@
 from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-import urllib
+
 
 app = Flask(__name__)
-app.config.from_object(Config)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://SEW-DESK/Firescrum'
-db = SQLAlchemy(app)
-
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-migrate = Migrate(app,db)
-
 from app import routes #, models <- to be implmented
